b35109285752872d61ccd3fb154fb0e130968db2
Suddani <suddani@googlemail.com>
2015-12-07 20:50:53 +0100

    moved stuff to shared

--- day7/day7.js
+++ day7/day7.js
@@ -1,20 +1,3 @@
-function modulo(a, b) {
-  return a - Math.floor(a/b)*b;
-}
-function ToInteger(x) {
-  x = Number(x);
-  return x < 0 ? Math.ceil(x) : Math.floor(x);
-}
-function ToUint32(x) {
-  return modulo(ToInteger(x), Math.pow(2, 32));
-}
-function ToUint16(x) {
-  return modulo(x, Math.pow(2, 16));
-}
-function isInt(n) {
-  return parseInt(n) === n;
-};
-
 function BASIC_COMMAND(name, regex, params, operation) {
   this.name = name;
   this.regex = regex;
